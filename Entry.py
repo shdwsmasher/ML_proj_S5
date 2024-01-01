7ccb44b0b4e384afef2b212a3c662d914e3ab9e7
Yash Arora <yasharora102@gmail.com>
2024-01-01 12:36:55 +0530

    Updated File Structure

---
+++ Entry.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from PIL import Image
+
+st.title("Housing Price Prediction of Indian Metro Cities")
+st.subheader(
+    "Use the sidebar to select the city and other features of the house to predict the price"
+)
+
+image = Image.open("house.jpg")
+st.image(image, caption="Housing Price Prediction", use_column_width=True)
